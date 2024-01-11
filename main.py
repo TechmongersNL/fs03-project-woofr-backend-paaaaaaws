2f3fc46f0a2bcff33313e3d03ac0765548ace91d
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2024-01-11 11:31:42 +0100

    add first endpoint #1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Woof!"}
