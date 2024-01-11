2c5a585c825cd5d82b7390e0d3241d1be29d342d
LileoC <LileoC@proton.me>
2024-01-11 14:10:33 +0100

    deleting and fetching woofs

--- main.py
+++ main.py
@@ -37,10 +37,21 @@ def get_db():
 # DELETE /woofs/{id} – to delete a Woof
 # GET /woofs – for the feed, so orderable by created_at, desc.
 
-@app.get("/")
-async def root():
-    return {"message": "Woof!"}
 
 @app.post("/woofs", response_model=schemas.Woof)
 def post_woof(woof: schemas.WoofCreate, db: Session = Depends(get_db)):
     return woofs.create_woof(db, woof=woof)
+
+@app.delete("/woofs{id}", response_model=schemas.Woof)
+def delete_woof_by_id(woof_id: int, db: Session = Depends(get_db)):
+    results = woofs.delete_woof(db, woof_id=woof_id)
+    if results is None:
+        raise HTTPException(status_code=404, detail="Woof not found")
+    return results
+
+@app.get("/woofs", response_model=list[schemas.Woof])
+def fetch_woofs(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+    results = woofs.get_woofs(db, skip=skip, limit=limit)
+    if results is None:
+        raise HTTPException(status_code=404, detail="No woofs found")
+    return results
