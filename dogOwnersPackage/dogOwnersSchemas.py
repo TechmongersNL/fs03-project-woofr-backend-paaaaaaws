c2abd9a1befd040670a2c1d7db0e24565ab99c35
AldrinKP <aldrinpastores@gmail.com>
2024-01-12 11:16:23 +0100

    #7 - Added authentication schemas to dogOwnersSchemas

--- dogOwnersPackage/dogOwnersSchemas.py
+++ dogOwnersPackage/dogOwnersSchemas.py
@@ -2,27 +2,44 @@ from typing import Union
 from pydantic import BaseModel
 
 
-
-#dog_owner base
+# dog_owner base
 class DogOwnerBase(BaseModel):
     id: int
     username: str
     about_me: str
-    
-    
-#create a dog owner
+    email: str
+
+
+# create a dog owner on sign up
 class DogOwnerCreate(BaseModel):
-    username: str
-    about_me: str
-    
-    
-#update dog owner about_me
+    email: str
+    password: str
+
+
+# credentials for dog owner
+class DogOwnerCredentials(DogOwnerCreate):
+    pass
+
+
+# update dog owner about_me
 class DogOwnerUpdate(BaseModel):
+    userame: str
     about_me: str
-    
-    
-#for one dog owner
+
+
+# for one dog owner
 class DogOwner(DogOwnerBase):
-    
+
     class Config:
         orm_mode = True
+
+
+# token pydantic models
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    exp: int
+    sub: str
