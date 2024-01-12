4a5d6a6c9bb1dde589e37563d81e7b681c86ef00
AldrinKP <aldrinpastores@gmail.com>
2024-01-12 11:07:16 +0100

    #7 - Added email and password hash column to table
    Co-authored-by: Lileo <LileoC@users.noreply.github.com>

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from app.database import Base
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 import sys
@@ -8,7 +9,6 @@ import datetime
 path = os.path.abspath("app")
 sys.path.append(path)
 
-from app.database import Base
 
 class Woof(Base):
     __tablename__ = "woofs"
@@ -16,7 +16,7 @@ class Woof(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     message = Column(String)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    
+
 
 class Dog_owner(Base):
     __tablename__ = "dog_owners"
@@ -24,6 +24,5 @@ class Dog_owner(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     username = Column(String, unique=True, index=True)
     about_me = Column(String)
- 
-
-
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
